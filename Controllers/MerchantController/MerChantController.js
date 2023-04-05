9de6872a57c1b208c01cb0dd14f171fa9fbcdb74
iam5abhi <support@codesoftic.com>
2023-04-05 15:06:40 +0530

    merchant singn up

--- Controllers/MerchantController/MerChantController.js
+++ Controllers/MerchantController/MerChantController.js
@@ -8,47 +8,55 @@ const createSendToken = require("../../suscribers/createSendToken");
 
 const CatchAsyncHandler =require('../../Middleware/Error/CatchAsyncHandler')
 
-// exports.CreateAccount = CatchAsyncHandler((req, res, next) => {
-//    const errors = validationResult(req);
-//    if (!errors.isEmpty())
-//       return res.status(422).send({ errors: errors.array() });
+ exports.CreateAccount = CatchAsyncHandler((req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+       return res.status(422).send({ errors: errors.array() });
 
-//    //Comparing The Password and confirmpassword
-//    if (
-//       base64.decode(req.body.password) != base64.decode(req.body.confirmPassword)
-//    )
-//       return next(new Error(PASSWORD_NOT_MATCH, 400));
+    //Comparing The Password and confirmpassword
+   if (
+      base64.decode(req.body.password) != base64.decode(req.body.confirmPassword)
+   )
+      return next(new Error(PASSWORD_NOT_MATCH, 400));
 
-//    User.findOne({ email: req.body.email }, (err, user) => {
-//       if (user)
-//          return next(new Error(USER_ALREADY_EXIST, 400));
-//    });
-//    let password = base64.decode(req.body.password);
-//    let confirmPassword = base64.decode(req.body.confirmPassword);
-//    const data = {
-//       name: req.body.name,
-//       email: req.body.email,
-//       phoneNumber: req.body.phoneNumber,
-//       password: password,
-//       confirmPassword: confirmPassword,
-//       city: req.body.city,
-//       state: req.body.state,
-//    };
+   User.findOne({ email: req.body.email }, (err, user) => {
+      if (user)
+         return next(new Error(USER_ALREADY_EXIST, 400));
+   });
+   let password = base64.decode(req.body.password);
+   let confirmPassword = base64.decode(req.body.confirmPassword);
+   const data = {
+    name: req.body.name,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    Alternate_Phone_Number:req.body.Alternate_Phone_Number,
+    address:[{
+       city:req.body.city,
+       state:req.body.state,
+       Office:req.body.office,
+       Building:req.body.Building,
+       Sector:req.body.Sector,
+       Pincode:parseInt(req.body.Pincode)
+ }],
+ GST:req.body.gstNumber,
+ password:password,
+ confirmPassword:confirmPassword
+ };
 
-//    const student = new User(data);
+   const student = new User(data);
 
-//    student.save(async(err, doc) => {
-//       if (err) return next(new Error(`${err.message}`, 400));
-//       const data  =await StudentProfile.create({StudentId:doc._id})
-//       res.status(200).send({
-//          message: REGISTRATION_SUCCESS,
-//          succes: true,
-//          name: doc.name,
-//          email: doc.email,
-//          phoneNumber: doc.phoneNumber,
-//       });
-//    });
-// });
+   student.save(async(err, doc) => {
+      if (err) return next(new Error(`${err.message}`, 400));
+      const data  =await StudentProfile.create({StudentId:doc._id})
+      res.status(200).send({
+         message: REGISTRATION_SUCCESS,
+         succes: true,
+         name: doc.name,
+         email: doc.email,
+         phoneNumber: doc.phoneNumber,
+      });
+   });
+});
 
 
 
